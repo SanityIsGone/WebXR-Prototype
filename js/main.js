0e7a03496272bd0f6425c228785399bb44d50320
SanityIsGone <timothytedder@icloud.com>
2026-09-17 08:38:59 -0400

    Update Cube Properties

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ if ( WebGL.isWebGL2Available() ) {
   const cubes = [
     makeInstance( geometry, 0x44aa88, 0, 0, 0 ),
     makeInstance( geometry, 0x8844aa, - 2, 0, 0 ),
-    makeInstance( geometry, 0xfafafa, 0, 0, 0 ),
+    makeInstance( geometry, 0x2a2a2a, 2, 0, 0 ),
   ];
   
   let prevTime = 0;
