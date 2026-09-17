56b3039c5a13f4ce11a61f81dbedb59fd91c2dda
SanityIsGone <timothytedder@icloud.com>
2026-09-16 21:56:40 -0400

    Clean Slate | Create Files
    Creates brand new test files for index.html and main.js

---
+++ js/main.js
@@ -0,0 +1,103 @@
+import * as THREE from 'three';
+import { WebGPURenderer } from 'three/webgpu';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if ( WebGL.isWebGL2Available() ) {
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width  = Math.floor( canvas.clientWidth  * pixelRatio );
+    const height = Math.floor( canvas.clientHeight * pixelRatio );
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+  
+  const stats = new Stats();
+  document.body.appendChild( stats.dom );
+  
+  //* =-=-=-=-=-=| SCENE SETUP |=-=-=-=-=-=
+  const canvas = document.querySelector('#c');
+
+  let renderer;
+  if (navigator.gpu) {
+    renderer = new WebGPURenderer({ canvas, antialias: true });
+  } else {
+    renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  }
+  
+  const camera = new THREE.PerspectiveCamera( 75, 2, 0.1, 999 );
+  camera.position.z = 5;
+  const controls = new OrbitControls( camera, renderer.domElement );
+  controls.update();
+  
+  const scene = new THREE.Scene();
+  
+  const light = new THREE.DirectionalLight( 0xFFFFFF, 3 );
+  light.position.set( 0, 1, 4 );
+  scene.add( light );
+  
+  const AmbientLight = new THREE.AmbientLight( 0xefefef ); // soft white light
+  scene.add( AmbientLight );
+  //* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+  
+  function makeInstance( geometry, color, x ) {
+    const material = new THREE.MeshPhongMaterial( { color } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    cube.position.x = x;
+    return cube;
+  }
+  
+  const cubes = [
+    makeInstance( geometry, 0x44aa88, 0 ),
+    makeInstance( geometry, 0x8844aa, - 2 ),
+    makeInstance( geometry, 0xaa8844, 2 ),
+  ];
+  
+  let prevTime = 0;
+  
+  //* =-=-=-=-=-=| RENDER LOOP |=-=-=-=-=-=
+  function render(time) {
+    time *= 0.001;
+    const deltaTime = time - prevTime;
+    prevTime = time;
+  
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+  
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = speed * deltaTime;
+      cube.rotation.x += rot;
+      cube.rotation.y += rot;
+    });
+  
+    navigator.gpu ? renderer.renderAsync(scene, camera) : renderer.render(scene, camera);
+    
+    stats.update();
+  
+    requestAnimationFrame(render);
+  }
+  
+  
+  requestAnimationFrame( render );
+  
+} else {
+  const warning = WebGL.getWebGL2ErrorMessage();
+  document.body.appendChild( warning );
+}
